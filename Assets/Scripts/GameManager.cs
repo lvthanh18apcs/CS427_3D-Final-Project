33a81de18a933485741f6335ef9f52b4854d0a5f
lvthanh18apcs <47595741+lvthanh18apcs@users.noreply.github.com>
2021-08-23 21:59:54 +0700

    backup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public bool freeze = false;
-    public bool hasLight = false;
+    bool plight = false;
+    bool hasLight = true;
     PlayerMovement movement;
     LookWithMouse mouseMovement;
     Light flashLight;
+    RaycastObj vision;
+    public Camera cam;
+    GameObject rotate_Obj;
 
     // Start is called before the first frame update
     void Start()
@@ -16,11 +20,49 @@ public class GameManager : MonoBehaviour
         movement = (PlayerMovement)gameObject.GetComponent(typeof(PlayerMovement));
         mouseMovement = (LookWithMouse)gameObject.GetComponentInChildren<Camera>().GetComponent(typeof(LookWithMouse));
         flashLight = (Light)gameObject.GetComponentInChildren<Camera>().GetComponent(typeof(Light));
+        vision = (RaycastObj)gameObject.GetComponent(typeof(RaycastObj));
+        cam = (Camera)gameObject.GetComponentInChildren(typeof(Camera));
+        rotate_Obj = null;
     }
+
+    public void viewObject(string name)
+    {
+        var canvas = (Canvas)gameObject.GetComponentInChildren(typeof(Canvas));
+        if (canvas == null)
+        {
+            Debug.Log("no");
+            return;
+        }
+        freeze = true;
+        var prefab = Resources.Load(name + ".prefab");
+        Vector3 pos = new Vector3(5, 0, 0) + cam.transform.position;
+        rotate_Obj = (GameObject)Instantiate(prefab, pos, Quaternion.identity);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            vision.handleInteration();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && rotate_Obj != null)
+        {
+            var canvas = (Canvas)gameObject.GetComponentInChildren(typeof(Canvas));
+            canvas.enabled = false;
+            freeze = false;
+            Destroy(rotate_Obj);
+            rotate_Obj = null;
+        }
+        if (Input.GetKeyDown(KeyCode.F) && hasLight)
+        {
+            if (plight)
+                plight = false;
+            else
+                plight = true;
+        }
+
         if (freeze)
         {
             movement.enabled = false;
@@ -32,7 +74,7 @@ public class GameManager : MonoBehaviour
             mouseMovement.enabled = true;
         }
 
-        if (hasLight)
+        if (plight)
             flashLight.enabled = true;
         else
             flashLight.enabled = false;
--- Assets/Scripts/RaycastObj.cs
+++ Assets/Scripts/RaycastObj.cs
@@ -8,13 +8,38 @@ public class RaycastObj : MonoBehaviour
     public string internalObject;
     public RaycastHit theObj;
     public GameObject obj;
+    GameManager manager;
+
+    private void Start()
+    {
+        manager = (GameManager)gameObject.GetComponent(typeof(GameManager));
+    }
+
     void Update()
     {
-        if(Physics.Raycast(transform.position,transform.TransformDirection(Vector3.forward),out theObj))
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out theObj, 30f))
         {
             selectedObj = theObj.transform.gameObject.name;
             internalObject = theObj.transform.gameObject.name;
             obj = theObj.transform.gameObject;
+        }
+        else
+        {
+            selectedObj = null;
+            internalObject = null;
+            obj = null;
+        }
+    }
+
+    public void handleInteration()
+    {
+        if (obj != null)
+        {
+            //rotate object
+            if (obj.transform.name == "DanceCode")
+            {
+                manager.viewObject("R_test");
+            }
         }
     }
 }
