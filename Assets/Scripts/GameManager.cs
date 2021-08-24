586366e84a497d4dab1afefee47ae1c3cf5155b4
lvthanh18apcs <47595741+lvthanh18apcs@users.noreply.github.com>
2021-08-24 21:55:56 +0700

    add StoryManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+enum Mode: int
+{
+    freeMode = 0,
+    viewMode = 1,
+    storyMode = 2
+}
+
 public class GameManager : MonoBehaviour
 {
     public bool freeze = false;
     bool plight = false;
     bool hasLight = true;
-    bool viewMode = false;
+    int cur_mode;
+
     PlayerMovement movement;
     LookWithMouse mouseMovement;
     Light flashLight;
@@ -16,8 +24,11 @@ public class GameManager : MonoBehaviour
     public UnityEngine.UI.Image background;
     public UnityEngine.UI.Text UIText;
     public UnityEngine.UI.Text modelText;
+    public UnityEngine.UI.Image storyPanel;
+    UnityEngine.UI.Text storyLine;
     GameObject rotate_Obj;
     LayerMask mask;
+    StoryManager story;
 
     // Start is called before the first frame update
     void Start()
@@ -27,16 +38,21 @@ public class GameManager : MonoBehaviour
         flashLight = (Light)gameObject.GetComponentInChildren<Camera>().GetComponent(typeof(Light));
         vision = (RaycastObj)gameObject.GetComponent(typeof(RaycastObj));
         cam = (Camera)gameObject.GetComponentInChildren(typeof(Camera));
+        story = (StoryManager)gameObject.GetComponent(typeof(StoryManager));
+        storyLine = storyPanel.GetComponentInChildren<UnityEngine.UI.Text>();
+        storyPanel.enabled = false;
+        storyLine.enabled = false;
         rotate_Obj = null;
         mask = cam.cullingMask;
         Cursor.lockState = CursorLockMode.Locked;
+        cur_mode = (int)Mode.freeMode;
     }
 
     public void viewObject(string name)
     {
         freeze = true;
         Cursor.visible = true;
-        viewMode = true;
+        cur_mode = (int)Mode.viewMode;
         Cursor.lockState = CursorLockMode.None;
         cam.cullingMask = ~mask;
 
@@ -44,24 +60,39 @@ public class GameManager : MonoBehaviour
         rotate_Obj = (GameObject)Instantiate(prefab,cam.transform.position + cam.transform.forward*3 , cam.transform.rotation);
     }
 
+    public void finishStory()
+    {
+        storyPanel.enabled = false;
+        storyLine.enabled = false;
+        freeze = false;
+        cur_mode = (int)Mode.freeMode;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !viewMode)
+        if (Input.GetMouseButtonDown(0) && cur_mode == (int)Mode.freeMode)
         {
             vision.handleInteration();
         }
-        else if (Input.GetKeyDown(KeyCode.F1) && viewMode)
+        else if (Input.GetKeyDown(KeyCode.Escape) && cur_mode == (int)Mode.viewMode)
         {
             freeze = false;
             Cursor.visible = false;
-            viewMode = false;
+            cur_mode = (int)Mode.freeMode;
             Cursor.lockState = CursorLockMode.Locked;
             cam.cullingMask = mask;
 
             Destroy(rotate_Obj);
             rotate_Obj = null;
+        }
+        else if (Input.GetKeyDown(KeyCode.F1) && cur_mode == (int)Mode.freeMode)
+        {
+            freeze = true;
+            cur_mode = (int)Mode.storyMode;
+            storyPanel.enabled = true;
+            storyLine.enabled = true;
+            story.runStory(storyLine, 0);
         }
         if (Input.GetKeyDown(KeyCode.F) && hasLight)
         {
